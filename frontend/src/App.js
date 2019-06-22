6b7bc2ce7e0c25cfc47a55044568cc5ac2f3c233
Faheem Patel <faheem@faheempatel.com>
2019-06-22 17:56:26 +0100

    Get auth working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 /*eslint no-undef: 0*/
 
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import styled from "@emotion/styled";
-import "./App.css";
+import React, { Component } from 'react';
+import styled from '@emotion/styled';
 
-import Header from "./components/Header";
-
-import gapi from "gapi-client";
+import Header from './components/Header';
 
 const Button = styled.button`
   background-color: blue;
@@ -19,30 +15,36 @@ const Button = styled.button`
   margin-top: 64px;
   color: white;
   font-size: 18px;
+  cursor: pointer;
 `;
 
 class App extends Component {
-  onSetupClick = () => {};
+  state = { gapiReady: false };
+
+  onSetupClick = async () => {
+    if (this.state.gapiReady) {
+      const googleAuth = await gapi.auth2.getAuthInstance();
+      googleAuth.signIn();
+    }
+  };
 
   componentDidMount() {
-    //On load, called to load the auth2 library and API client library.
-    // gapi.load("client:auth2", initClient);
-    console.log("hello");
-    // Initialize the API client library
-    function initClient() {
-      gapi.client
-        .init({
-          clientId: process.env.CLIENT_ID,
-          scope:
-            "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events"
-        })
-        .then(function() {
-          // do stuff with loaded APIs
-          console.log("it worked");
-        })
-        .catch(() => console.log("failed"));
-    }
-    gapi.load("client:auth2", initClient);
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/platform.js';
+
+    script.onload = () => {
+      gapi.load('auth2', () => {
+        this.setState({ gapiReady: true }, () => {
+          gapi.auth2.init({
+            client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+            scope:
+              'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events'
+          });
+        });
+      });
+    };
+
+    document.body.appendChild(script);
   }
 
   render() {
