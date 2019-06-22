2715ef9d71948ce33385296c62764f6125acc963
Faheem Patel <faheem@faheempatel.com>
2019-06-22 23:11:52 +0100

    Add button component

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  width: 300px;
+  height: 50px;
+  padding: 10px 20px;
+
+  background-color: black;
+  border: none;
+  border-radius: 150px;
+
+  font-family: 'work sans', sans-serif;
+  font-size: 1.125rem;
+  font-weight: bold;
+  color: white;
+
+  cursor: pointer;
+`;
+
+export default Button;
