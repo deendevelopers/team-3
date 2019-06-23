8436960f1945fbc0ea025050a1257a0ac81779dc
Faheem Patel <faheem@faheempatel.com>
2019-06-23 16:34:28 +0100

    Move options file

--- frontend/src/OptionsPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MiddleOfPage from './components/MiddleOfPage';
-
-const OptionsPage = () => {
-  return <MiddleOfPage>Options Page</MiddleOfPage>;
-};
-
-export default OptionsPage;
