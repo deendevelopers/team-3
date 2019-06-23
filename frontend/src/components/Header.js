cc2077d324303688ddf48caa6202f9b57d86db11
Faheem Patel <faheem@faheempatel.com>
2019-06-23 16:05:52 +0100

    Move typography styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,26 +7,6 @@ const Hero = styled.div`
   max-width: 28em;
   padding: 32px;
   text-align: left;
-
-  h1 {
-    font-family: 'anton', sans-serif;
-    text-transform: uppercase;
-    font-size: 6em;
-    letter-spacing: 0.025em;
-    line-height: 1;
-  }
-
-  h2 {
-    font-size: 2.25rem;
-    color: #5198d6;
-    letter-spacing: -0.01em;
-  }
-
-  p {
-    font-size: 1.125rem;
-    line-height: 1.6;
-    color: #2e3036;
-  }
 `;
 
 const Header = ({ children, ...props }) => <Hero>{children}</Hero>;
